8ba54d099c9050d5653309a316c2360f383fe439
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2022-12-05 14:30:44 -0600

    working day 4 stuff

--- src/main.rs
+++ src/main.rs
@@ -332,6 +332,59 @@ fn rucksack_badge_search(files: &str) -> i32 {
     return pri_sum;
 }
 
+fn find_complete_overlaps(file: &str) -> i32 {
+    let mut overlaps: i32 = 0;
+    if let Ok(lines) = read_lines(file) {
+        for line in lines {
+            let line_str = line.unwrap();
+            let (left, right) = line_str.as_str().split_once(',').unwrap();
+            let (s1, e1) = left.split_once('-').unwrap();
+            let (s2, e2) = right.split_once('-').unwrap();
+            let s1_int = s1.parse::<i32>().unwrap();
+            let e1_int = e1.parse::<i32>().unwrap();
+            let s2_int = s2.parse::<i32>().unwrap();
+            let e2_int = e2.parse::<i32>().unwrap();
+
+            if s1_int <= s2_int && e1_int >= e2_int {
+                overlaps += 1;
+            } else if s1_int >= s2_int && e1_int <= e2_int {
+                overlaps += 1;
+            }
+        }
+    }
+
+    println!("Overlapping job pairs: {}", overlaps);
+    return overlaps;
+}
+
+fn find_partial_overlaps(file: &str) -> i32 {
+    let mut overlaps: i32 = 0;
+    if let Ok(lines) = read_lines(file) {
+        for line in lines {
+            let line_str = line.unwrap();
+            let (left, right) = line_str.as_str().split_once(',').unwrap();
+            let (s1, e1) = left.split_once('-').unwrap();
+            let (s2, e2) = right.split_once('-').unwrap();
+            let s1_int = s1.parse::<i32>().unwrap();
+            let e1_int = e1.parse::<i32>().unwrap();
+            let s2_int = s2.parse::<i32>().unwrap();
+            let e2_int = e2.parse::<i32>().unwrap();
+
+            if s1_int <= s2_int && e1_int >= s2_int {
+                overlaps += 1;
+                continue;
+            }
+            if s1_int >= s2_int && s1_int <= e2_int {
+                overlaps += 1;
+                continue;
+            }
+        }
+    }
+
+    println!("Overlapping job pairs: {}", overlaps);
+    return overlaps;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -344,6 +397,8 @@ fn main() {
             "d2-2" => _ = rock_paper_scissors2("./day2.txt"),
             "d3" => _ = rucksack_sorting("./day3.txt"),
             "d3-2" => _ = rucksack_badge_search("./day3.txt"),
+            "d4" => _ = find_complete_overlaps("./day4.txt"),
+            "d4-2" => _ = find_partial_overlaps("./day4.txt"),
             _ => println!("Unrecognized command."),
         }
     } else {
@@ -379,5 +434,15 @@ mod tests {
         let priority_sum = rucksack_badge_search("./day3test.txt");
 
         assert_eq!(priority_sum, 70)
+    }
+    #[test]
+    fn test_day4_1() {
+        let overlaps = find_complete_overlaps("./day4test.txt");
+        assert_eq!(overlaps, 2)
+    }
+    #[test]
+    fn test_day4_2() {
+        let overlaps = find_partial_overlaps("./day4test.txt");
+        assert_eq!(overlaps, 4)
     }
 }
