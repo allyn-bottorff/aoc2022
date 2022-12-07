bbccaf416da1f5b5002892e404ba20bbb430ed6c
Allyn L. Bottorff <abottorff@gmail.com>
2022-12-06 19:37:02 -0600

    working day 6, both parts

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,22 +1,68 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs;
 
 /// Read lines from a file
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
 
 fn find_packet_start(file: &str) -> usize {
-    7
+    let signal_string = fs::read_to_string(file).unwrap();
+    let signal = signal_string.as_str();
+
+    let signal_length = signal.len();
+    let mut start_index: usize = 0;
+    let mut end_index: usize = start_index + 4;
+    while end_index < signal_length {
+        let char_quad = &signal[start_index..end_index];
+
+        if characters_are_unique(char_quad) {
+            //println!("found unique set: {}", char_quad);
+            return end_index;
+        }
+        start_index += 1;
+        end_index += 1;
+    }
+
+    return 0;
+}
+fn find_message_start(file: &str) -> usize {
+    let signal_string = fs::read_to_string(file).unwrap();
+    let signal = signal_string.as_str();
+
+    let signal_length = signal.len();
+    let mut start_index: usize = 0;
+    let mut end_index: usize = start_index + 14;
+    while end_index < signal_length {
+        let char_quad = &signal[start_index..end_index];
+
+        if characters_are_unique(char_quad) {
+            //println!("found unique set: {}", char_quad);
+            return end_index;
+        }
+        start_index += 1;
+        end_index += 1;
+    }
+
+    return 0;
+}
+
+fn characters_are_unique(char_quad: &str) -> bool {
+    for c1 in char_quad.chars() {
+        let mut found_count: i32 = 0;
+        for c2 in char_quad.chars() {
+            if c1 == c2 {
+                found_count += 1;
+            }
+            if found_count > 1 {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 fn main() {
-    println!("Hello, world!");
+    let packet_start = find_packet_start("./day6.txt");
+    println!("Found packet start at position: {}", packet_start);
+    let message_start = find_message_start("./day6.txt");
+    println!("Found message start at position: {}", message_start);
 }
 
 #[cfg(test)]
@@ -27,5 +73,16 @@ mod tests {
     fn test_find_packet_start_1() {
         let packet_start = find_packet_start("./tests/day6-1.txt");
         assert_eq!(packet_start, 7)
+    }
+
+    #[test]
+    fn test_characters_are_not_unique() {
+        let char_quad = "abad";
+        assert_eq!(characters_are_unique(char_quad), false)
+    }
+    #[test]
+    fn test_characters_are_unique() {
+        let char_quad = "abec";
+        assert_eq!(characters_are_unique(char_quad), true)
     }
 }
