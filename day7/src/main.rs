df7f15de8b6242a329a70b797a240ae604a69ace
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2022-12-12 15:56:44 -0600

    working day 7 part 1, part 2 not working yet

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,17 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 #[derive(Debug, Default)]
-struct ArenaTree<T>
-where
-    T: PartialEq,
-{
-    arena: Vec<Node<T>>,
+struct ArenaTree {
+    arena: Vec<Node>,
 }
-impl<T> ArenaTree<T>
-where
-    T: PartialEq,
-{
-    fn node(&mut self, val: T) -> usize {
+impl ArenaTree {
+    fn node(&mut self, val: String) -> usize {
         for node in &self.arena {
             if node.val == val {
                 return node.index;
@@ -25,18 +19,64 @@ where
         index
     }
 
-    fn get_children<'a>(
-        &'a self,
-        index: usize,
-        mut child_list: &'a mut Vec<usize>,
-    ) -> &mut Vec<usize> {
-        child_list.extend_from_slice(&self.arena[index].children);
-        for child in &self.arena[index].children {
-            child_list = self.get_children(*child, child_list);
+    fn get_full_path_prefix(&self, starting_index: usize) -> String {
+        let mut names: Vec<&str> = Vec::new();
+        let mut node: usize = starting_index;
+        let mut full_path: String = String::from("");
+        loop {
+            names.push(&self.arena[node].val);
+            node = match &self.arena[node].parent {
+                Some(node) => *node,
+                None => break,
+            };
+        }
+        for name in names {
+            full_path = format!("{}{}", full_path, name);
+        }
+        full_path
+    }
+
+    fn get_children(&self, starting_index: usize) -> Vec<usize> {
+        let mut all_children: Vec<usize> = Vec::new();
+        let mut nodes: Vec<usize> = Vec::new();
+        let mut first_node: bool = true;
+        nodes.push(starting_index);
+
+        loop {
+            let node = match nodes.pop() {
+                Some(node) => node,
+                None => break,
+            };
+
+            for child in &self.arena[node].children {
+                if all_children.contains(child) {
+                    // println!("found_loop at index {}", child);
+                    break;
+                }
+                nodes.push(*child)
+            }
+            if first_node == false {
+                all_children.push(node)
+            } else {
+                first_node = false;
+            }
         }
 
-        child_list
+        all_children
     }
+
+    // fn get_children_2<'a>(
+    //     &'a self,
+    //     index: usize,
+    //     mut child_list: &'a mut Vec<usize>,
+    // ) -> &mut Vec<usize> {
+    //     child_list.extend_from_slice(&self.arena[index].children);
+    //     for child in &self.arena[index].children {
+    //         child_list = self.get_children_2(*child, child_list);
+    //     }
+
+    //     child_list
+    // }
 
     // fn size(&self) -> usize {
     //     self.arena.len()
@@ -44,22 +84,16 @@ where
 }
 
 #[derive(Debug)]
-struct Node<T>
-where
-    T: PartialEq,
-{
+struct Node {
     index: usize,
-    val: T,
+    val: String,
     files: Vec<i32>,
     parent: Option<usize>,
     children: Vec<usize>,
 }
 
-impl<T> Node<T>
-where
-    T: PartialEq,
-{
-    fn new(index: usize, val: T) -> Self {
+impl Node {
+    fn new(index: usize, val: String) -> Self {
         Self {
             index,
             files: vec![],
@@ -85,15 +119,16 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn process_file(file: &str) -> ArenaTree<String> {
+fn process_file(file: &str) -> ArenaTree {
     let lines = read_lines(file).unwrap();
-    let mut tree: ArenaTree<String> = ArenaTree::default();
+    let mut tree: ArenaTree = ArenaTree::default();
     let root = tree.node("/".into());
 
     let mut current_dir: usize = root;
     for line_result in lines {
         let line = line_result.unwrap();
         let line_type = parse_line(line);
+        let parent_dir_name = &tree.arena[current_dir].val.to_string();
         match line_type {
             LineType::CD(dir_name) => {
                 if dir_name == ".." {
@@ -101,15 +136,17 @@ fn process_file(file: &str) -> ArenaTree<String> {
                         Some(dir) => dir,
                         None => root,
                     }
+                } else if dir_name == "/" {
+                    current_dir = root;
                 } else {
-                    current_dir = tree.node(dir_name);
+                    current_dir = tree.node(format!("{}{}/", parent_dir_name, dir_name));
                 }
             }
-            LineType::LS => {
-                continue;
-            }
+            LineType::LS => {}
             LineType::Dir(dir_name) => {
-                let found_dir = tree.node(dir_name);
+                // let parent_dir_name = &tree.arena[current_dir].val.to_string();
+                let found_dir = tree.node(format!("{}{}/", parent_dir_name, dir_name));
+                // let found_dir = tree.node(dir_name);
                 tree.arena[found_dir].parent = Some(current_dir);
                 tree.arena[current_dir].children.push(found_dir);
             }
@@ -121,14 +158,14 @@ fn process_file(file: &str) -> ArenaTree<String> {
     tree
 }
 
-fn get_recursive_dir_size(tree: &ArenaTree<String>, directory: usize) -> i32 {
+fn get_recursive_dir_size(tree: &ArenaTree, directory: usize) -> i32 {
     let mut total_size: i32 = 0;
-    let mut children: Vec<usize> = Vec::new();
+    // let mut children: Vec<usize> = Vec::new();
 
     for file in &tree.arena[directory].files {
         total_size += *file;
     }
-    children = tree.get_children(directory, &mut children).to_vec();
+    let children = tree.get_children(directory);
     for child in children {
         for file in &tree.arena[child].files {
             total_size += *file;
@@ -138,7 +175,7 @@ fn get_recursive_dir_size(tree: &ArenaTree<String>, directory: usize) -> i32 {
     total_size
 }
 
-fn get_total_size(tree: &ArenaTree<String>) -> i32 {
+fn get_total_size(tree: &ArenaTree) -> i32 {
     let mut total_size: i32 = 0;
 
     for node in &tree.arena {
@@ -169,15 +206,57 @@ fn parse_line(line: String) -> LineType {
 fn main() {
     println!("Hello, world!");
 
+    // let fs_tree = process_file("./tests/day7.txt");
+    // println!("{:?}", fs_tree);
+
     let fs_tree = process_file("./day7.txt");
 
-    println!("{}", get_total_size(&fs_tree));
-    println!("{}", get_recursive_dir_size(&fs_tree, 0));
+    let children = fs_tree.get_children(0);
 
-    // for dir in &fs_tree.arena {
-    //     let dir_size = get_recursive_dir_size(&fs_tree, dir.index);
-    //     println!("Directory name: {:?} size: {:?}", dir.val, dir_size)
-    // }
+    let full_path = fs_tree.get_full_path_prefix(10);
+    println!("full path from node index 10: {}", full_path);
+
+    println!("total child dirs: {}", children.len());
+    let total_size: i32 = get_total_size(&fs_tree);
+    println!("total size directly: {}", total_size);
+    println!(
+        "total size recursively: {}",
+        get_recursive_dir_size(&fs_tree, 0)
+    );
+
+    let mut at_most_100k_dirs: Vec<i32> = Vec::new();
+    let mut frees_enough_space_dirs: Vec<i32> = Vec::new();
+
+    let required_space_to_free: i32 = 70000000 - get_total_size(&fs_tree);
+
+    for dir in &fs_tree.arena {
+        let dir_size = get_recursive_dir_size(&fs_tree, dir.index);
+        if dir_size < 100000 {
+            at_most_100k_dirs.push(dir_size);
+        }
+        if dir_size > required_space_to_free {
+            frees_enough_space_dirs.push(dir_size);
+        }
+
+        println!("Directory name: {:?} size: {:?}", dir.val, dir_size)
+    }
+
+    let mut big_dir_sum: i32 = 0;
+    for dir in at_most_100k_dirs {
+        big_dir_sum += dir;
+    }
+
+    println!("required space to free: {}", required_space_to_free);
+    println!("sum of big dirs: {}", big_dir_sum);
+
+    frees_enough_space_dirs.sort();
+    println!("acceptable_directories: {:?}", frees_enough_space_dirs);
+    println!(
+        "smallest acceptable directory: {}",
+        frees_enough_space_dirs[0]
+    );
+    let smallest = frees_enough_space_dirs.pop().unwrap();
+    println!("largest acceptable directory: {}", smallest);
 }
 
 #[cfg(test)]
@@ -192,7 +271,7 @@ mod tests {
     }
 
     #[test]
-    fn test_get_recursive_sizze() {
+    fn test_get_recursive_size() {
         let fs_tree = process_file("./tests/day7.txt");
         let total_size = get_recursive_dir_size(&fs_tree, 0);
         assert_eq!(total_size, 48381165)
@@ -201,12 +280,18 @@ mod tests {
     #[test]
     fn test_get_tree_children() {
         let fs_tree = process_file("./tests/day7.txt");
-        let mut children: Vec<usize> = Vec::new();
-        children = fs_tree.get_children(0, &mut children).to_vec();
+        let mut children: Vec<usize> = fs_tree.get_children(0);
 
         println!("{:?}", children);
 
-        assert_eq!(children, vec![1, 2, 3])
+        assert_eq!(children.sort(), vec![1, 2, 3].sort())
+    }
+
+    #[test]
+    fn test_full_path() {
+        let fs_tree = process_file("./tests/day7.txt");
+        let full_path = fs_tree.get_full_path_prefix(3);
+        println!("full path? {}", full_path);
     }
 
     // #[test]
