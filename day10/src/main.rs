c10bb24c3f8e69f8d52a14edb91b6e7f42006eae
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2022-12-22 15:44:23 -0600

    working day 10 pt 1 and 2, there might be a bug in the pixel drawing logic around a corner case at the end of the lines

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -26,6 +26,7 @@ fn process_file(filename: &str) -> Vec<i32> {
                 if check_for_important_clocks(&clock, &regx) {
                     signals.push(clock * regx);
                 }
+                draw_pixel(&clock, &regx);
                 clock += 1;
                 continue;
             }
@@ -33,11 +34,13 @@ fn process_file(filename: &str) -> Vec<i32> {
                 if check_for_important_clocks(&clock, &regx) {
                     signals.push(clock * regx);
                 }
+                draw_pixel(&clock, &regx);
                 let add_val = line_vec[1].parse::<i32>().unwrap();
                 clock += 1;
                 if check_for_important_clocks(&clock, &regx) {
                     signals.push(clock * regx);
                 }
+                draw_pixel(&clock, &regx);
                 clock += 1;
                 regx += add_val;
                 continue;
@@ -49,20 +52,52 @@ fn process_file(filename: &str) -> Vec<i32> {
     signals
 }
 
-fn check_for_important_clocks(clock: &i32, regx: &i32) -> bool {
+fn draw_pixel(clock: &i32, regx: &i32) {
+    if (*clock % 40) - 1 == *regx - 1
+        || (*clock % 40) - 1 == *regx
+        || (*clock % 40) - 1 == *regx + 1
+    {
+        print!("#");
+    } else {
+        print!(".");
+    }
+
     match clock {
-        20 => println!("Clock: {}, X: {}", clock, regx),
-        60 => println!("Clock: {}, X: {}", clock, regx),
-        100 => println!("Clock: {}, X: {}", clock, regx),
-        140 => println!("Clock: {}, X: {}", clock, regx),
-        180 => println!("Clock: {}, X: {}", clock, regx),
-        220 => println!("Clock: {}, X: {}", clock, regx),
+        40 => println!(""),
+        80 => println!(""),
+        120 => println!(""),
+        160 => println!(""),
+        200 => println!(""),
+        240 => println!(""),
+        _ => {}
+    }
+}
+
+fn check_for_important_clocks(clock: &i32, _regx: &i32) -> bool {
+    // match clock {
+    //     20 => println!("Clock: {}, X: {}", clock, regx),
+    //     60 => println!("Clock: {}, X: {}", clock, regx),
+    //     100 => println!("Clock: {}, X: {}", clock, regx),
+    //     140 => println!("Clock: {}, X: {}", clock, regx),
+    //     180 => println!("Clock: {}, X: {}", clock, regx),
+    //     220 => println!("Clock: {}, X: {}", clock, regx),
+    //     _ => return false,
+    // }
+    match clock {
+        20 => return true,
+        60 => return true,
+        100 => return true,
+        140 => return true,
+        180 => return true,
+        220 => return true,
         _ => return false,
     }
-    true
 }
 
 fn main() {
+    let test_signals = process_file("./tests/day10.txt");
+    println!("");
+
     let signals = process_file("./day10.txt");
 
     let sum: i32 = signals.iter().sum();
