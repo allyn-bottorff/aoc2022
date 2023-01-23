be73dbf1e73697793bd27c6b14587e8c80907eba
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-01-23 15:25:15 -0600

    stash

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -6,17 +6,19 @@ struct Monkey {
     items: Vec<i32>,
     true_dest: i32,
     false_dest: i32,
-    operation: fn(i32) -> i32,
+    operation: fn(i32, i32) -> i32,
+    op_const: i32,
     test: fn(i32) -> bool,
 }
 impl Monkey {
     fn new() -> Self {
-        let operation = |x: i32| -> i32 { x + 3 };
+        let operation = add_two;
         let test = |x: i32| -> bool { x % 10 == 0 };
         Self {
             true_dest: 0,
             false_dest: 0,
             operation: operation,
+            op_const: 0,
             items: vec![0],
             test: test,
         }
@@ -31,10 +33,29 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn add_two(old: i32, c: i32) -> i32 {
+    old + c
+}
+
+fn mult_two(old: i32, c: i32) -> i32 {
+    old * c
+}
+
+fn sub_two(old: i32, c: i32) -> i32 {
+    old - c
+}
+
 fn process_file(filename: &str) {
     let mut monkeys: Vec<Monkey> = Vec::new();
     let lines = read_lines(filename);
 
+
+    let mut starting_items: Vec<i32> = Vec::new();
+    let mut operation: fn(i32, i32) -> i32  = add_two;
+    let mut test: fn(i32) -> bool = |x: i32| -> bool {true};
+    let mut true_dest: usize = 0;
+    let mut false_dest: usize = 0;
+let mut op_const: i32 = 0;
     for line_result in lines.unwrap() {
         let line_string = line_result.unwrap();
         let line_vec: Vec<&str> = line_string.split_whitespace().collect();
@@ -44,19 +65,47 @@ fn process_file(filename: &str) {
         }
 
         match line_vec[0] {
-            "Monkey" => monkeys.push(Monkey::new()),
+            "Monkey" => continue,
             "Starting" => {
                 for item in line_vec {
                     let num_result = item.parse::<i32>();
                     match num_result {
                         Ok(x) => {
-                            let monkey = &mut *monkeys.last().unwrap();
-                            monkey.items.push(x);
+                            starting_items.push(x);
                         }
                         Err(_) => {}
                     }
                 }
             }
+            "Operation:" =>  {
+                let constant_result = line_vec[5].parse::<i32>();
+                match constant_result {
+                    Ok(c) => {
+                        match line_vec[4] {
+                            "+" => {
+                    operation = &add_two;
+                    
+
+}
+                            "-" => operation = &sub_two,
+                            "*" => operation = &mult_two,
+                        },
+                    Err(_) => {
+            match line_vec[4] {
+                            "+" => operation = |x: i32| -> i32 {x + x},
+                            "-" => operation = |x: i32| -> i32 {x - x},
+                            "*" => operation = |x: i32| -> i32 {x * x},
+
+            }
+        }
+
+                    }
+                }
+                match line_vec[4] {
+                    "+"
+                }
+            }
+            
         }
     }
 }
